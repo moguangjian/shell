e8811ab85948086b393e7cb8e724388a839fc819
moguangjian <13639098303@139.com>
2019-07-03 15:10:53 +0800

    在centos7中安装python3、uwsgi、nginx，并将需要使用python2的地方进行修改

---
+++ ins_py3_uwsgi_nginx.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# 本脚本用于安装pytthon3 、uwsgi、nginx
+
+# 更新源
+yum install -y epel-release 
+yum install -y https://centos7.iuscommunity.org/ius-release.rpm 
+yum clean all 
+yum makecache 
+yum -y upgrade 
+
+yum-builddep -y python 
+yum install -y python36u  
+yum install -y python36u-devel  
+yum install -y python36u-pip      
+rm -f /usr/bin/python  
+rm -f /usr/bin/pip  
+ln -s /bin/python3.6 /usr/bin/python 
+ln -s /bin/pip3.6 /usr/bin/pip 
+pip install --no-cache-dir --upgrade pip -i https://pypi.douban.com/simple  
+if ! grep "python2" /usr/bin/yum  >/dev/null 2>&1;then
+    sed -i 's/python/python2.7/' /usr/bin/yum
+fi
+if ! grep "python2" /usr/bin/yum-builddep  >/dev/null 2>&1;then
+    sed -i 's/python/python2.7/' /usr/bin/yum-builddep
+fi
+if ! grep "python2" /usr/bin/yum-config-manager  >/dev/null 2>&1;then
+    sed -i 's/python/python2.7/' /usr/bin/yum-config-manager
+fi
+if ! grep "python2" /usr/bin/yum-debug-dump  >/dev/null 2>&1;then
+    sed -i 's/python/python2.7/' /usr/bin/yum-debug-dump
+fi
+if ! grep "python2" /usr/bin/yum-debug-restore  >/dev/null 2>&1;then
+    sed -i 's/python/python2.7/' /usr/bin/yum-debug-restore
+fi
+if ! grep "python2" /usr/bin/yum-groups-manager  >/dev/null 2>&1;then
+    sed -i 's/python/python2.7/' /usr/bin/yum-groups-manager
+fi
+if ! grep "python2" /usr/bin/yumdownloader  >/dev/null 2>&1;then
+    sed -i 's/python/python2.7/' /usr/bin/yumdownloader
+fi
+if ! grep "python2" /usr/libexec/urlgrabber-ext-down  >/dev/null 2>&1;then
+    sed -i 's/python/python2.7/' /usr/libexec/urlgrabber-ext-down
+fi
+#sed -i 's/python/python2.7/' /usr/bin/yum  
+#sed -i 's/python/python2.7/' /usr/bin/yum-builddep  
+#sed -i 's/python/python2.7/' /usr/bin/yum-config-manager 
+#sed -i 's/python/python2.7/' /usr/bin/yum-debug-dump 
+#sed -i 's/python/python2.7/' /usr/bin/yum-debug-restore 
+#sed -i 's/python/python2.7/' /usr/bin/yum-groups-manager 
+#sed -i 's/python/python2.7/' /usr/bin/yumdownloader 
+#sed -i 's/python/python2.7/' /usr/libexec/urlgrabber-ext-down 
+
+pip install --no-cache-dir uwsgi -i https://pypi.douban.com/simple
+yum install nginx -y 
+yum clean all
