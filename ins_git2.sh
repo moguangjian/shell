2196ed39b274c735a5d945267dcc319f631e1a68
gdlmo <gztf@21cn.com>
2019-10-23 12:07:07 +0800

    增加了一些依赖包，并且修正了windows下编辑的文件与linux不兼容的地方

--- ins_git2.sh
+++ ins_git2.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+# 本脚本用于安装git 2.7 服务
 cd /usr/src
-yum -y install curl-devel wget perl-ExtUtils-MakeMaker package
-service httpd restart
-wget https://Github.com/Git/Git/archive/v2.7.3.tar.gz
+yum -y install gcc-c++ openssl-devel curl-devel expat-devel libcurl-dev libcurl-devell wget perl-ExtUtils-MakeMaker package lftp
+# systemctl restart httpd
+# wget https://Github.com/Git/Git/archive/v2.7.3.tar.gz
+lftp -c "pget -n 10 https://Github.com/Git/Git/archive/v2.7.3.tar.gz"
 tar xzf v2.7.3.tar.gz
 cd git-2.7.3
 make prefix=/usr/local/git all
